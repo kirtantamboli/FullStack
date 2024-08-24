dc6c1b3c36f473780dfd6287f8b09195f92fc244
Kirtan Tamboli <kirtantamboli38@gmail.com>
2024-08-24 14:48:47 +0530

    js

---
+++ DAY_8/javascript.js
@@ -0,0 +1,49 @@
+        console.log('js');
+        console.log('script');
+
+        //value and container
+        //data-types
+        //NOTE datdtypes
+        //NOTE PRIMITIVE
+        /*string
+        number
+        boolean
+        undefined
+        bigint
+        symbol*/
+        //NOTE NON-PRIMITIVE
+        /*
+        array()
+        object()
+         */
+        //STRING
+        //variable declaration
+        //always use camelcase
+
+        let firstname ='kirtan';
+        console.log(firstname);
+        console.log(typeof firstname);
+        //NOTE NUMBER TYPE
+        let age = 20;
+        console.log(typeof age)
+        //NOTE BOOLEAN TYPE
+        let isjsfun =true;
+        console.log(typeof isjsfun)
+        //NOTE UNDEFINED
+        let college;
+        console.log(college)
+        console.log(typeof college)
+        //NOTE NULL
+        let department = null;
+        console.log(typeof college);
+
+        var lastname ='singh';
+        lastname ='sharma';
+        console.log(lastname); 
+        var fruits
+        console.log(fruits)
+        //note yes - reassign no-redeclare
+        let company ='meta';
+        company='netflix';
+        console.log(company);
+          
