8ff9223a68d21f14b742b568b70fffae16caf38b
Kirtan Tamboli <kirtantamboli38@gmail.com>
2024-08-29 14:37:04 +0530

    .

---
+++ R_E_A_C_T/app.jsx
@@ -0,0 +1,17 @@
+import React from "./button.jx"
+
+function app(){
+    const firstname = 'abc'
+    return(
+        <div>
+            <button/>
+            <h1 style={{color:'red',backgroundColor:'blue'}}>REACT</h1>
+            <input maxLength={5}/>
+        </div>
+    )
+}
+// export const app = () => {
+//   return (
+//     <div>app</div>
+//   )
+// }
