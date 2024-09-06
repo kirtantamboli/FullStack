0e68e55a89913d694fbf31b19e94580aff5816ec
Kirtan Tamboli <kirtantamboli38@gmail.com>
2024-09-06 14:04:07 +0530

    .

--- NODE.JS/server.js
+++ NODE.JS/server.js
@@ -1,16 +1,28 @@
 const http = require('http');
 const server = http.createServer((req,res)=>
-{
+{   
+    console.log(req.url);
+    if(req.url==='/about'){
+        res.end('about pages');
+    }
+    else if(req.url === '/services'){
+        res.end('service page');
+    }
+    else{
+        res.end('home page');
+    }
+    
     res.end("data is send from the server");
-})
+});
 
 server.listen(3000,()=>{
     console.log("Server is running");
     
 })
+http => request , response 
 // through port system's application is differentiated 
 // host means system's ip
-http => request , response 
+
 // request as backend
 // response as frontend 
 
