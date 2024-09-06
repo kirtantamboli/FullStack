82542ab027591ab064df0fd52674c4a606e6ae43
Kirtan Tamboli <kirtantamboli38@gmail.com>
2024-09-06 14:55:45 +0530

    .

--- NODE.JS/server.js
+++ NODE.JS/server.js
@@ -1,29 +1,34 @@
 const http = require('http');
-const server = http.createServer((req,res)=>
-{   
-    console.log(req.url);
-    if(req.url==='/about'){
-        res.end('about pages');
-    }
-    else if(req.url === '/services'){
-        res.end('service page');
-    }
-    else{
-        res.end('home page');
-    }
-    
-    res.end("data is send from the server");
+const path = require('path');
+const fs = require('fs');
+const server = http.createServer((req, res) => {
+  console.log(req.url);
+  if (req.url === '/about') {
+    const filePath = path.join(__dirname, 'about.html');
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        res.end('err');
+      } else {
+        res.writeHead(200,{'Content-Type':'text/html'})
+        res.end(data);
+      }
+    });
+
+    //NOTE always use writeHead method before sending the response
+  } 
+  else if (req.url === '/services') {
+    res.end('service page');
+  } else {
+    // res.end('home page');
+  }
 });
 
-server.listen(3000,()=>{
-    console.log("Server is running");
-    
-})
-http => request , response 
-// through port system's application is differentiated 
-// host means system's ip
+server.listen(3000, () => {
+  console.log('Server is running');
+});
 
-// request as backend
-// response as frontend 
+// http => request  , response
 
-//localhost 3000 in webbrowser 
+// npm i nodemon -g --save-dev
+
+//methods
