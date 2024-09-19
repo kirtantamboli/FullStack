8a3c6fd0a4f9c44401980cdbb258fc6b6f3d702d
Kirtan Tamboli <kirtantamboli38@gmail.com>
2024-09-19 14:52:44 +0530

    added

--- PROJECT/frontend/src/PAGES/SIGNUP.jsx
+++ PROJECT/frontend/src/PAGES/SIGNUP.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
 import {useForm} from 'react-hook-form'
+import z from 'zod'
+
+import {zodResolver} from '@hookform/resolvers/zod'
+
 function Signup(){
-  const {register , handleSubmit}=useForm()
+  //schema for error show
+  const schema = z.object({
+    name : z.string().min(1,"name is required").max(40,"name cannot exceed 40 characters")})
+
+  const {register , formState : {errors},handleSubmit} = useForm({resolver:zodResolver(schema)});
   const onSubmit =(data)=>{
     console.log(data);
   }
@@ -10,6 +18,16 @@ function Signup(){
       <form onSubmit={handleSubmit(onSubmit)}>
         <label htmlFor='name'>Name</label>
         <input type ='text'{...register('name')}/>
+
+        <label htmlFor='phone'>PHONE</label>
+        <input type ='number'{...register('PHONE')}/>
+
+        <label htmlFor='email'>EMAIL</label>
+        <input type='email'{...register('email')}/>
+
+        <label htmlFor='password'>password</label>
+        <input type ='password'{...register('name')}/>
+        
         <button type='submit'>Signup</button>
       </form>
     </div>
