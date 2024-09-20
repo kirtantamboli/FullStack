2bc80daf6a4ff318111b5596f2ecb94c956383e6
Kirtan Tamboli <kirtantamboli38@gmail.com>
2024-09-20 13:38:06 +0530

    added

--- PROJECT/frontend/src/App.jsx
+++ PROJECT/frontend/src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import Signup from './PAGES/SIGNUP'
-import {Routes , Route} from 'react-router-dom'
+import Signup from './pages/Signup'
+import { Routes, Route } from 'react-router-dom'
 const App = () => {
   return (
     <Routes>
-      <Route path ="/register" element = {<Signup />} />
+      <Route path="/register" element={<Signup />} />
     </Routes>
   )
 }
--- PROJECT/frontend/src/PAGES/SIGNUP.jsx
+++ PROJECT/frontend/src/PAGES/SIGNUP.jsx
@@ -1,35 +1,40 @@
 import React from 'react'
 import {useForm} from 'react-hook-form'
-import z from 'zod'
-
+import {z} from 'zod'
 import {zodResolver} from '@hookform/resolvers/zod'
+const Signup = () => {
 
-function Signup(){
-  //schema for error show
-  const schema = z.object({
-    name : z.string().min(1,"name is required").max(40,"name cannot exceed 40 characters")})
+    const schema = z.object({
+        name : z.string().min(1,"Name is Required").max(40,"name cannot exceed 40 characters"),
+        email : z.string().email('email is required'),
+        phone : z.string().min(10,"min 10 digit is Required"),
+        password : z.string().min(8,"password must contain 8 digits and characters").regex(/[0-9]/,"password must contain atleast one num").regex(/[A-Z]/,'password must contain atleat one uppercase').regex(/[a-z]/,"password must contain atleast one lowercase").regex(/[\w_]/,"password must contain atleat one special symbol"),
+    })
 
-  const {register , formState : {errors},handleSubmit} = useForm({resolver:zodResolver(schema)});
-  const onSubmit =(data)=>{
-    console.log(data);
-  }
+    const{register , formState:{errors} , handleSubmit} = useForm({
+        resolver:zodResolver(schema)
+    })
+    const onSubmit = (data) =>{
+        console.log(data);
+    }
   return (
     <div>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <label htmlFor='name'>Name</label>
-        <input type ='text'{...register('name')}/>
+        <form onSubmit={handleSubmit(onSubmit)} className='flex'>
+            <label htmlFor="name">Name</label>
+            <input type="text" {...register('name')}/>
+            {errors.name && <p className='text-red-500'>{errors.name.message}</p>}
 
-        <label htmlFor='phone'>PHONE</label>
-        <input type ='number'{...register('PHONE')}/>
+            <label htmlFor="email">Email</label>
+            <input type="email" {...register('email')}/>
 
-        <label htmlFor='email'>EMAIL</label>
-        <input type='email'{...register('email')}/>
+            <label htmlFor="phone">Phone</label>
+            <input type="tel" {...register('phone')}/>
 
-        <label htmlFor='password'>password</label>
-        <input type ='password'{...register('name')}/>
-        
-        <button type='submit'>Signup</button>
-      </form>
+            <label htmlFor="password">Password</label>
+            <input type="password" {...register('phone')}/>
+
+            <button type="submit">Signup</button>
+        </form>
     </div>
   )
 }
