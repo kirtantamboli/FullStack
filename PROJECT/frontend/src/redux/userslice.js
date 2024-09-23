b659c592daa40c7113f18bdd3296cb8478f45f60
Kirtan Tamboli <kirtantamboli38@gmail.com>
2024-09-23 14:00:35 +0530

    .

--- PROJECT/frontend/src/redux/userslice.js
+++ PROJECT/frontend/src/redux/userslice.js
@@ -1,8 +1,9 @@
 import { createSlice, rejectedWithValue } from "@reduxjs/toolkit";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from 'axios';
+import { toast } from "sonner";
 
-export const register = createAsyncThunk('/user/login',async(data,{rejectWithValue})=>{
+export const Register = createAsyncThunk('/user/login',async(data,{rejectWithValue})=>{
     try{
     const res = await axios.post('http://localhost:3000/api/regiter',data) ;
     return res.data
@@ -22,14 +23,14 @@ const userSlice = createSlice({
     extraReducers :(builder)=>{
         //first pending
         //handling api -- pending fulfilled rejected
-        builder.addCase(register.pending,(state)=>{
+        builder.addCase(Register.pending,(state)=>{
             state.loading = true
-        }).addCase(register.fulfilled,(state)=>{
+        }).addCase(Register.fulfilled,(state)=>{
             state.loading = false
-        }).addCase(register.rejected,(state,action)=>{
+        }).addCase(Register.rejected,(state,action)=>{
             console.log(action.payload);
             state.error = action.payload
-            
+            toast.error(action.payload.response.data.message)
         })
 
     }
