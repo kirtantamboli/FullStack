78eaaf91996ee4bc4c402520e91b67d15e2d56a7
Kirtan Tamboli <kirtantamboli38@gmail.com>
2024-09-25 14:30:26 +0530

    .

--- PROJECT/backend/routes/userRoutes.js
+++ PROJECT/backend/routes/userRoutes.js
@@ -1,10 +1,13 @@
 //express import 
 const express = require('express') ;
-const userController = require('../controllers/userController.js')
+const userController = require('../controllers/userController.js');
+const passport = require('passport');
 //express.Router()
 const router = express.Router();
 
 router.post('/register',userController.register) ;
 router.post('/login',userController.login);
-
+router.get('/auth/google',passport.authenticate('google',[{scope :['profile',email]}]))
 module.exports = router ;
+
+router.get('/auth/google/callback',passport.authenticate('google',failureRedirect)
